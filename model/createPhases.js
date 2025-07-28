ee1a8d0285890b5d6326204844675fad53520b1b
Pedro Pinheiro Guedes <pedroguedes96@hotmail.com>
2025-07-28 23:08:11 +0200

    consertando query psql

--- model/createPhases.js
+++ model/createPhases.js
@@ -12,35 +12,30 @@ const db = new Pool({
 
 export function createPhases(){
     db.query(`
-        insert into phases (word, hint1, hint2, hint3)
-            VALUES ( 
-                    'ENGENHARIA SOCIAL',
+        insert if not exists into phases (word, hint1, hint2, hint3)
+            VALUES ('ENGENHARIA SOCIAL',
                     'Tem como base induzir vítimas a revelar informações ou realizar ações sem perceber o risco.',
                     'A arte de exploram a confiança para convencer alguém a compartilhar dados ou dar acesso a sistemas.',
                     'Phishing, Baiting, Pretexting, Spear Phishing estao entre suas principais tecnicas'
                     ),
-                    ( 
-                    'PHISHING',
+                    ('PHISHING',
                     'Tem como base induzir vítimas a revelar informações ou realizar ações sem perceber o risco.',
                     'A base da engenharia social: induzir vítimas a revelar informações ou realizar ações sem per.',
                     'A base da engenharia social: induzir vítimas a revelar informações ou realizar ações sem per.'
                     ),
-                    ( 
-                    'PHISHING',
+                    ('PHISHING',
                     'Tem como base induzir vítimas a revelar informações ou realizar ações sem perceber o risco.',
                     'A base da engenharia social: induzir vítimas a revelar informações ou realizar ações sem per.',
                     'A base da engenharia social: induzir vítimas a revelar informações ou realizar ações sem per.'
                     ),
-                    ( 
-                    'PHISHING',
+                    ('PHISHING',
                     'Tem como base induzir vítimas a revelar informações ou realizar ações sem perceber o risco.',
                     'A base da engenharia social: induzir vítimas a revelar informações ou realizar ações sem per.',
                     'A base da engenharia social: induzir vítimas a revelar informações ou realizar ações sem per.'
                     ),
-                    ( 
-                    'PHISHING',
+                    ('PHISHING',
                     'Tem como base induzir vítimas a revelar informações ou realizar ações sem perceber o risco.',
                     'A base da engenharia social: induzir vítimas a revelar informações ou realizar ações sem per.',
                     'A base da engenharia social: induzir vítimas a revelar informações ou realizar ações sem per.'
-                    );`
+                    ) ON CONFLICT (id) DO NOTHING;;`
 )}
